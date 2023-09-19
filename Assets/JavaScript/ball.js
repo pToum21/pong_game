03c5c0654a5892ca5c537835f1b92a615d454eed
toum21 <pey2moo1@gmail.com>
2023-09-18 17:12:45 -0700

    working on the ball js

--- Assets/JavaScript/ball.js
+++ Assets/JavaScript/ball.js
@@ -1,3 +1,5 @@
+const INITIAL_VELOCITY = 0.025;
+
 export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem
@@ -5,31 +7,42 @@ export default class Ball {
 
 
     get x() {
-        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue('--x'));
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
     }
     set x(value) {
-        this.ballElem.style.setProperty('--x', value);
+        this.ballElem.style.setProperty("--x", value)
     }
 
 
 
     get y() {
-        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue('--y'));
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
     }
     set y(value) {
-        this.ballElem.style.setProperty('--y', value);
+        this.ballElem.style.setProperty("--y", value)
     }
 
     reset() {
         this.x = 50;
-        this.y =50;
-        this.direction = {x: 0.75, y: 0.5};
+        this.y = 50;
+        this.direction = { x: 0 };
+
+        while (
+            Math.abs(this.direction.x) <= 0.2 ||
+            Math.abs(this.direction.x) >= 0.9) {
+            const heading = randomNumberBetween(0, 2 * Math.PI)
+            this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
+        }
+        this.velocity = INITIAL_VELOCITY
+        
     }
-
-
 
     update(delta) {
-        this.x = 5;
-        this.y =15;
+        this.x += this.direction.x * this.velocity * delta
+        this.y += this.direction.y * this.velocity * delta
     }
+}
+
+function randomNumberBetween(min, max) {
+    return Math.random() * (max - min) + min;
 }
